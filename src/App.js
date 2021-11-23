e42466be4c8be5ac20dcb72acda87aed29272f4b
wafa rtibi <wafertibi@gmail.com>
2021-11-23 16:34:31 +0100

    checkpoint react intro

--- src/App.js
+++ src/App.js
@@ -1,24 +1,69 @@
-import logo from './logo.svg';
 import './App.css';
-
+import{Form,Row,Button,Col} from 'react-bootstrap'
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+   <div id="boxx">
+     <h1>Welcome</h1>
+    <Form>
+    <Row className="mb-3">
+    <Form.Group as={Col} controlId="formGridEmail">
+      <Form.Label>Email</Form.Label>
+      <Form.Control type="email" placeholder="Enter email" />
+    </Form.Group>
+
+    <Form.Group as={Col} controlId="formGridPassword">
+      <Form.Label>Password</Form.Label>
+      <Form.Control type="password" placeholder="Password" />
+    </Form.Group>
+    </Row>
+
+    <Form.Group className="mb-3" controlId="formGridAddress1">
+    <Form.Label>Address</Form.Label>
+    <Form.Control placeholder="1234 Main St" />
+   </Form.Group>
+
+
+  <Row className="mb-3">
+    <Form.Group as={Col} controlId="formGridCity">
+      <Form.Label>City</Form.Label>
+      <Form.Control />
+    </Form.Group>
+
+    <Form.Group as={Col} controlId="formGridState">
+      <Form.Label>State</Form.Label>
+      <Form.Select defaultValue="Choose...">
+        <option>Tunisie</option>
+        <option>France</option>
+      </Form.Select>
+    </Form.Group>
+
+    <Form.Group as={Col} controlId="formGridZip">
+      <Form.Label> Postal code</Form.Label>
+      <Form.Control />
+    </Form.Group>
+  </Row>
+
+  <Form.Group className="mb-3" id="formGridCheckbox">
+    <Form.Check type="checkbox" label="i accept the rules" />
+  </Form.Group>
+
+  <Button variant="primary" type="submit">
+  Sign Up
+  </Button>
+  <div id="icons">
+     <i class="fab fa-facebook-square"></i>
+     <i class="fab fa-twitter-square"></i>
+     <i class="fab fa-instagram"></i>
+     </div>
+  
+  </Form>
+   
+  </div>
+
     </div>
+   
   );
 }
 
